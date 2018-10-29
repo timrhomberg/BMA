b21ea5ec3f432fcfb8380086e0dc3ea44422a598
timrhomberg <timrhomberg@hotmail.com>
2018-10-29 21:46:52 +0100

    Microphone Test erweitert

--- MicrophoneTest.py
+++ MicrophoneTest.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python3
 import speech_recognition as sr
 
-r = sr.Recognizer()
-with sr.Microphone(device_index=2) as source:
-    print("Höre zu...")
-    audio = r.listen(source)
+while True:
+    r = sr.Recognizer()
+    with sr.Microphone(device_index=2) as source:
+        print("Höre zu...")
+        audio = r.listen(source)
 
-try:
-    vartext = ''
-    vartext = r.recognize_google(audio, language="de_DE")
-    if (vartext == 'start') or (vartext == 'starten'):
-        print ('Auto fährt gerade los')
-    elif (vartext == 'rechts abbiegen') or (vartext == 'rechts'):
-        print('Auto biegt rechts ab')
-    else:
-        print(vartext)
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+    try:
+        vartext = ''
+        vartext = r.recognize_google(audio, language="de_DE")
+        if (vartext == 'start') or (vartext == 'starten') or (vartext == 'losfahren'):
+            print ('Auto fährt gerade los')
+        elif (vartext == 'rechts abbiegen') or (vartext == 'rechts'):
+            print('Auto biegt rechts ab')
+        elif (vartext == 'links abbiegen') or (vartext == 'links'):
+            print('Auto biegt links ab')
+        else:
+            print(vartext)
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
