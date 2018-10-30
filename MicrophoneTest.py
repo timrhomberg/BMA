69de363c7c6e1bbe2772712f62dce7472638143c
timrhomberg <timrhomberg@hotmail.com>
2018-10-30 22:40:37 +0100

    Microphone Test erweitert

--- MicrophoneTest.py
+++ MicrophoneTest.py
@@ -1,7 +1,40 @@
 #!/usr/bin/env python3
 import speech_recognition as sr
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+import time
+import atexit
+
+def MotorenAbschalten():
+    mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+    mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+def geradeFahren():
+    MotorPort4.run(Adafruit_MotorHAT.FORWARD)
+    MotorPort4.setSpeed(100)
+
+def rueckwaertsFahren():
+    MotorPort4.run(Adafruit_MotorHAT.BACKWARD)
+    MotorPort4.setSpeed(100)
+
+def rechtsFahren():
+    MotorPort4.run(Adafruit_MotorHAT.FORWARD)
+    MotorPort4.setSpeed(100)
+    MotorPort3.run(Adafruit_MotorHAT.FORWARD)
+    MotorPort3.setSpeed(50)
+
+def linksFahren():
+    MotorPort4.run(Adafruit_MotorHAT.FORWARD)
+    MotorPort4.setSpeed(100)
+    MotorPort3.run(Adafruit_MotorHAT.BACKWARD)
+    MotorPort3.setSpeed(50)
+
+atexit.register(MotorenAbschalten)
 
 while True:
+    mh = Adafruit_MotorHAT(addr=0x60)
+    MotorPort3 = mh.getMotor(3)
+    MotorPort4 = mh.getMotor(4)
+
     r = sr.Recognizer()
     with sr.Microphone(device_index=2) as source:
         print("Höre zu...")
