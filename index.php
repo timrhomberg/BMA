66b3e1154d7d01d98a7c7e5279a2eea033c1f26b
timrhomberg <timrhomberg@hotmail.com>
2018-10-31 23:35:28 +0100

    index.php erweitert

--- index.php
+++ index.php
@@ -29,14 +29,25 @@
 <html>
     <head>
         <title>Sensor Daten</title>
+        <script src="http://code.jquery.com/jquery-2.1.3.js" type="text/javascript"></script>
+        <script>
+            $(document).ready(function() {
+                   $("#refresh").load("db_laden.php");
+                   var refreshId = setInterval(function() {
+                      $("#refresh").load("db_laden.php");
+                   }, 7000);
+                });
+        </script>
     </head>
     <body>
-        <h1>Ultraschall Sensor Daten</h1>
-        <p>Sensor Vorne: <?php echo "<b>$varvorne</b>"; ?> </p>
-        <p>Sensor Hinten: <?php echo "<b>$varhinten</b>"; ?>  </p>
-        <figure>
-            <img src="auto-vogelperspektive.jpg" alt="Auto aus Vogelperspektive.">
-            <figcaption>Quellenangabe <a href="https://techflourish.com/categories/auto-von-oben-clipart.html">Link</a></figcaption>
-        </figure>
+        <div id="refresh">
+            <h1>Ultraschall Sensor Daten</h1>
+            <p>Sensor Vorne: <?php echo "<b>$varvorne</b>"; ?> </p>
+            <p>Sensor Hinten: <?php echo "<b>$varhinten</b>"; ?>  </p>
+            <figure>
+               <img src="auto-vogelperspektive.jpg" alt="Auto aus Vogelperspektive.">
+               <figcaption>Quellenangabe <a href="https://techflourish.com/categories/auto-von-oben-clipart.html">Link</a></figcaption>
+            </figure>
+        </div>
     </body>
 </html>
