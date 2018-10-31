3932c8785f39fb65ad35b1b1debc117f6363c2bf
timrhomberg <timrhomberg@hotmail.com>
2018-10-31 23:39:28 +0100

    index.php erweitert

--- index.php
+++ index.php
@@ -44,10 +44,7 @@
             <h1>Ultraschall Sensor Daten</h1>
             <p>Sensor Vorne: <?php echo "<b>$varvorne</b>"; ?> </p>
             <p>Sensor Hinten: <?php echo "<b>$varhinten</b>"; ?>  </p>
-            <figure>
-               <img src="auto-vogelperspektive.jpg" alt="Auto aus Vogelperspektive." style="float:right;>
-               <figcaption>Quellenangabe <a href="https://techflourish.com/categories/auto-von-oben-clipart.html">Link</a></figcaption>
-            </figure>
+            <img src="auto-vogelperspektive.jpg" alt="Auto aus Vogelperspektive." style="float:right;>
         </div>
     </body>
 </html>
