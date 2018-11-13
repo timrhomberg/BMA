26e7ed786ed95c2778901a7acc1e2a2561650dbf
timrhomberg <timrhomberg@hotmail.com>
2018-11-13 22:55:05 +0100

    MicrophoneTest erweitert

--- ControlSensor.py
+++ ControlSensor.py
@@ -3,14 +3,13 @@ import redis
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
-
+mh = Adafruit_MotorHAT(addr=0x60)
+MotorPort1 = mh.getMotor(1)
+MotorPort2 = mh.getMotor(2)
 while True:
     # Sensor Daten aus Redis DB auslesen
     varvorne = r.get('vorne')
     varhinten = r.get('hinten')
-    # Definition Motor Objekt
-    mh = Adafruit_MotorHAT(addr=0x60)
-    MotorPort1 = mh.getMotor(1)
-    MotorPort2 = mh.getMotor(2)
+
     if (varvorne <= '4') or (varhinten <= '4'):
         MotorPort1.setSpeed(0)
